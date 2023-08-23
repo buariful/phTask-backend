15e79cea58e98bb4c3e74f55ff2fa1426e6466d8
ariful <arifulislam321054@gmail.com>
2023-08-23 22:33:05 +0600

    update

--- app.js
+++ app.js
@@ -5,10 +5,9 @@ const cors = require("cors");
 const postRouter = require("./router/post.router");
 
 const app = express();
-
 app.use(bodyParser.urlencoded({ limit: "10mb", extended: true }));
 app.use(bodyParser.json({ limit: "20mb", extended: true }));
-app.use(cors({ origin: process.env.FRONTEND_URL, credentials: true }));
+app.use(cors());
 app.use("/api/v1", postRouter);
 
 app.get("/", (_req, res) => {
